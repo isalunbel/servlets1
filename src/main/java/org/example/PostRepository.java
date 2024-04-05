87fdb8069b5fec8eae61056c354dd72a5a1ed646
isalunbel <142603867+isalunbel@users.noreply.github.com>
2024-04-05 18:15:34 +0300

    Update PostRepository.java

--- src/main/java/org/example/PostRepository.java
+++ src/main/java/org/example/PostRepository.java
@@ -8,4 +8,9 @@ public interface PostRepository {
     Optional<Post> getById(long id);
     Post save(Post post);
     void removeById(long id);
+
+    static final String ALL_METHOD_NAME = "all";
+    static final String GET_BY_ID_METHOD_NAME = "getById";
+    static final String SAVE_METHOD_NAME = "save";
+    static final String REMOVE_BY_ID_METHOD_NAME = "removeById";
 }
