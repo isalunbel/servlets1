6946e28900f844de4bddfe77b72ca3eb8ab65b16
isalunbel <142603867+isalunbel@users.noreply.github.com>
2024-04-05 18:24:05 +0300

    Update PostRepositoryImpl.java

--- src/main/java/org/example/PostRepositoryImpl.java
+++ src/main/java/org/example/PostRepositoryImpl.java
@@ -1,23 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class PostRepositoryImpl implements PostRepository {
-    private final List<Post> posts = new CopyOnWriteArrayList<>();
-    private final AtomicLong postIdCounter = new AtomicLong(0);
     private static final String POST_NOT_FOUND_MESSAGE = "Post with id %d not found";
+
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong postIdCounter = new AtomicLong(0);
 
     @Override
     public List<Post> all() {
-        return posts;
+        return new ArrayList<>(posts.values());
     }
 
     @Override
     public Optional<Post> getById(long id) {
-        return posts.stream().filter(post -> post.getId() == id).findFirst();
+        return Optional.ofNullable(posts.get(id));
     }
 
     @Override
@@ -25,21 +27,20 @@ public class PostRepositoryImpl implements PostRepository {
         if (post.getId() == 0) {
             long newId = postIdCounter.incrementAndGet();
             post.setId(newId);
-            posts.add(post);
+            posts.put(newId, post);
             return post;
         } else {
-            for (int i = 0; i < posts.size(); i++) {
-                if (posts.get(i).getId() == post.getId()) {
-                    posts.set(i, post);
-                    return post;
-                }
+            if (posts.containsKey(post.getId())) {
+                posts.put(post.getId(), post);
+                return post;
+            } else {
+                throw new IllegalArgumentException(String.format(POST_NOT_FOUND_MESSAGE, post.getId()));
             }
-            throw new IllegalArgumentException(String.format(POST_NOT_FOUND_MESSAGE, post.getId()));
         }
     }
 
     @Override
     public void removeById(long id) {
-        posts.removeIf(post -> post.getId() == id);
+        posts.remove(id);
     }
 }
